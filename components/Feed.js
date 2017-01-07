51a2a7d23b50eb95e94f78a5f9834c530be84924
brianmc <brian.mcauley.rails@gmail.com>
2017-01-07 12:44:31 -0500

    working listveiw with api call

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,8 @@
 'use strict';
 
 import React, {Component} from 'react';
-import {Text, View, ListView, StyleSheet} from 'react-native';
+import {Text, View, ListView, StyleSheet, ActivityIndicator, Image} from 'react-native';
+import moment from 'moment';
 
 class Feed extends Component {
     constructor(props) {
@@ -11,14 +12,31 @@ class Feed extends Component {
             rowHasChanged: (r1, r2) => r1 != r2
         });
         this.state = {
-            dataSource: ds.cloneWithRows(['a','b','c']),
+            dataSource: ds,
+            showProgress: true
 
         };
     }
     renderRow(rowData){
-        return <Text style={styles.rowData}>
-                    {rowData}
-                </Text>
+        return(
+          <View style={styles.rowData}>
+              <Image
+                  source={{uri: rowData.actor.avatar_url}}
+                  style={styles.rowImage}/>
+              <View style={styles.rowInfo}>
+                  <Text style={styles.rowInformation}>
+                      {moment(rowData.created_at).fromNow()}
+                  </Text>
+                  <Text style={styles.rowInformation}>
+                      {rowData.actor.login}
+                  </Text>
+                  <Text style={styles.rowInformation}>
+                      {rowData.repo.name}
+                  </Text>
+
+              </View>
+          </View>
+        );
     }
     componentDidMount(){
         this.fetchFeed();
@@ -26,8 +44,8 @@ class Feed extends Component {
 
     fetchFeed(){
         require('./AuthService.js').getAuthInfo((err, authInfo)=>{
-           var url = 'https://api.github.com/users/'
-               + authInfo.user.login
+           var url = 'https://api.github.com/users/hendrikswan'
+               //+ authInfo.user.login
                + '/received_events';
 
            fetch(url, {
@@ -35,26 +53,38 @@ class Feed extends Component {
            })
            .then((response)=> response.json())
            .then((responseData)=>{
-
-               var feedItems =
-                   responseData.filter((ev)=>
-                   ev.type=='PushEvent');
+               var feedItems = 
+                   responseData.filter((ev)=> 
+                   ev.type=='ForkEvent');
                this.setState({
                    dataSource: this.state.dataSource
-                       .cloneWithRows(feedItems)
+                       .cloneWithRows(feedItems),
+                   showProgress: false
                });
+               console.log(this.state.showProgress)
            })
         });
     }
     render() {
-        return (
-            <View style={styles.container}>
-                <ListView
-                    dataSource={this.state.dataSource}
-                    renderRow={this.renderRow.bind(this)}/>
-            </View>
-
-        );
+        if(this.state.showProgress) {
+            return (
+                <View style={styles.loadercontainer}>
+                    <ActivityIndicator
+                        animating={true}
+                        size="large"
+                        style={styles.loader}
+                    />
+                </View>
+            );
+        }else {
+            return (
+                <View style={styles.container}>
+                    <ListView
+                        dataSource={this.state.dataSource}
+                        renderRow={this.renderRow.bind(this)}/>
+                </View>
+            );
+        }
     }
 }
 
@@ -62,12 +92,35 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'flex-start',
+        marginTop: 20
     },
     rowData: {
-        color: '#333',
-        backgroundColor: '#fff',
-        alignSelf: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        padding: 20,
+        alignItems: 'center',
+        borderColor: '#D7D7D7',
+        borderBottomWidth: 1,
     },
+    loader: {
+        marginTop: 20,
+    },
+    loadercontainer: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    rowImage: {
+        height: 36,
+        width: 36,
+        borderRadius: 18,
+    },
+    rowInfo: {
+        paddingLeft: 20,
+    },
+    rowInformation: {
+        backgroundColor: '#FFF',
+        fontWeight: '600',
+    }
 });
 
 module.exports = Feed;
